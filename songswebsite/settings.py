f5ea688ab875ab2faf90940e5cf7956cf4dd751e
Boina Eswarsai <eswarsai458@gmail.com>
2025-08-30 23:10:25 +0530

    Fix deployment files

--- songswebsite/settings.py
+++ songswebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*%gz_w_yzvrh3+7bykdekbycrl)hc*696=t^%vyv=c%666#_jg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
