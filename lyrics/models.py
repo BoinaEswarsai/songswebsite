45b363398955af3671c88966b448a59f0c662d08
BoinaEswarsai <eswarsai458@gmail.com>
2025-08-31 17:25:55 +0530

    Update models.py

--- lyrics/models.py
+++ lyrics/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=200)
     lyrics = models.TextField()
-    artist = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.title
