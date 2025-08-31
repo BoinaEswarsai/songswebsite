6b3fda0b962d245352ea32307868baf35efc924d
BoinaEswarsai <eswarsai458@gmail.com>
2025-08-31 13:04:49 +0530

    Update models.py

--- lyrics/models.py
+++ lyrics/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Song(models.Model):
-    title = models.CharField(max_length=200, unique=True)   # పాట పేరు
-    lyrics = models.TextField()                             # సాహిత్యం
+    title = models.CharField(max_length=200)
+    lyrics = models.TextField()
+    artist = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.title
