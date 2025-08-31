1d327aa20b8ac55212ef44cfb7881bf282f4e2c8
BoinaEswarsai <eswarsai458@gmail.com>
2025-08-31 17:23:41 +0530

    Update urls.py

--- songswebsite/urls.py
+++ songswebsite/urls.py
@@ -1,12 +1,11 @@
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from lyrics import views
 
+ 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('songs/', views.song_list, name='song_list'),
     path('songs/<int:song_id>/', views.song_detail, name='song_detail'),
+    path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
